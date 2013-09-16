d1eeb51bf07b9f5d97ef228e0290ca2561e3e2c0
joshsprague <josh (at) newsplay.org>
2013-09-15 19:09:37 -0700

    Cleaned up commented out code

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-//Set map tile
-	// var map = L.map('map');
-	// 	L.tileLayer('http://{s}.tile.cloudmade.com/a1ed8ecdfd8e41d890fc74beb07cdc63/997/256/{z}/{x}/{y}.png', {
-	// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-	// 	maxZoom: 18
-	// }).addTo(map);
 var layer = new L.StamenTileLayer("toner-lite");
 var map = new L.Map("map", {
   center: new L.LatLng(37.75, -122.45),
